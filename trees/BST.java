95cce058c6af87e7999faa76b6aae5e5d9a7f6a9
Sumit Patel <sumit826942@gmail.com>
2026-09-19 17:20:24 +0530

    Add search method to BST class and update main for key search

--- trees/BST.java
+++ trees/BST.java
@@ -35,6 +35,16 @@ public class BST {
         inorder(root.right);
     }
 
+    public static boolean search(Node root, int key){
+        if(root==null) return false;
+        if(root.data == key) return true;
+        if(root.data>key){
+            return search(root.left, key);
+        }else{
+            return search(root.right, key);
+        }
+    }
+
     public static void main(String[] args) {
         int values[]={5,1,3,4,2,7};
         Node root = null;
@@ -46,5 +56,11 @@ public class BST {
         System.out.println("Inorder traversal of the BST:");
         inorder(root);
         System.out.println();
+
+        if (search(root, 21)) {
+            System.out.println("found");
+        }else{
+            System.out.println("no found");
+        }
     }
 }
