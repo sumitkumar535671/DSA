bf7cd8d19aaa186f7b00bddbe3efe708ff86227f
Sumit Patel <sumit826942@gmail.com>
2026-09-22 21:57:46 +0530

    Implement delete method in BST class and add findInorderSuccessor helper method

--- trees/BST.java
+++ trees/BST.java
@@ -45,6 +45,38 @@ public class BST {
         }
     }
 
+    public static Node delete(Node root, int val){
+        if(root.data>val){
+            root.left = delete(root.left, val);
+        }else if(root.data<val){
+            root.right = delete(root.right, val);
+        }else{
+            //case 1
+            if(root.left==null && root.right==null){
+                return null;
+            }
+            //case 2
+            if(root.left==null){
+                return root.right;
+            }else if(root.right==null){
+                return root.left;
+            }
+
+            //case 3
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node findInorderSuccessor(Node root){
+        while(root.left!=null){
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
         int values[]={5,1,3,4,2,7};
         Node root = null;
